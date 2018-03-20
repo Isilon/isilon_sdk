65fbe0a06293b4920c7d92f88da1fc887ca8c517
Brandon Krueger <Brandon.Krueger@isilon.com>
2018-03-20 13:47:21 -0700

    Update dnscache schema

--- components/create_swagger_config.py
+++ components/create_swagger_config.py
@@ -248,10 +248,7 @@ def isi_schema_to_swagger_object(isi_obj_name_space, isi_obj_name,
         if 'properties' not in isi_schema and 'settings' not in isi_schema:
             log.warning(('Invalid empty schema for object %s. '
                          "Adding 'properties' and 'type'."), isi_obj_name)
-            schema_copy = isi_schema.copy()
-            for key in isi_schema.keys():
-                del isi_schema[key]
-            isi_schema['properties'] = schema_copy
+            isi_schema = {'properties': isi_schema}
         else:
             log.warning(("Invalid schema for object %s, no 'type' specified. "
                          "Adding 'type': 'object'."), isi_obj_name)
@@ -425,11 +422,7 @@ def isi_schema_to_swagger_object(isi_obj_name_space, isi_obj_name,
                 log.warning("Move 'properties' into the 'items' object")
         elif sub_obj_namespace.startswith('SummaryProtocolStatsProtocol'):
             if 'type' not in prop:
-                prop_copy = prop.copy()
-                for key in prop.keys():
-                    del prop[key]
-                prop['properties'] = prop_copy
-                prop['type'] = 'object'
+                prop = {'properties': prop, 'type': 'object'}
                 log.warning("Move properties into the 'properties' object")
             elif prop_name == 'protocol' and prop['type'] == 'array':
                 prop['type'] = 'object'
@@ -457,7 +450,10 @@ def isi_schema_to_swagger_object(isi_obj_name_space, isi_obj_name,
             if prop_name == 'status' and 'enum' in prop:
                 del prop['enum']
                 log.warning("Remove invalid 'status' enum")
-
+        elif sub_obj_namespace == 'NetworkDnscache':
+            if prop_name == 'settings' and 'items' in prop:
+                prop = prop['items']
+                log.warning("Property 'settings' is an object, not an array")
 
         if 'type' not in prop:
             if 'enum' in prop:
@@ -1367,7 +1363,7 @@ def main():
     else:
         exclude_end_points = []
         end_point_paths = [
-            ('/3/network/interfaces', None),
+            ('/3/network/dnscache', None),
         ]
 
     success_count = 0
